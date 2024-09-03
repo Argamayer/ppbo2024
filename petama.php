cdac6456a71bbaf75d32f9b6717c902808ddbd72
Argamayer <160709033+Argamayer@users.noreply.github.com>
2024-09-03 21:29:50 +0700

    Praktikum 2b - Menggunakan Fungsi Bawaan

--- petama.php
+++ petama.php
@@ -1,4 +1,17 @@
 <?php
-    echo "Hello World"
+date_default_timezone_set("Asia/Jakarta");
+$nama = readline('Masukkan nama Anda: ');
+$waktu = date('H:i');
 
-?>
+// Menentukan sapaan berdasarkan waktu saat ini
+if ($waktu >= 5 && $waktu < 11) {
+    $sapaan = "Selamat pagi";
+} elseif ($waktu >= 11 && $waktu < 15) {
+    $sapaan = "Selamat siang";
+} elseif ($waktu >= 15 && $waktu < 19) {
+    $sapaan = "Selamat sore";
+} else {
+    $sapaan = "Selamat malam";
+}
+
+echo "{$sapaan}, {$nama}, sekarang pukul {$waktu}\n"; 
