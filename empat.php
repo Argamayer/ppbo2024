78c8e4e4c1071dcc258aa0a0420a6ea428c5c661
Argamayer <160709033+Argamayer@users.noreply.github.com>
2024-09-06 16:08:34 +0700

    Praktikum 5B sampai 5E Penggunaan Require dan Include

--- empat.php
+++ empat.php
@@ -1,83 +1,8 @@
 <?php
-class Author {
-    public $name;
-    public $description;
 
-    public function __construct($name, $description) {
-        $this->name = $name;
-        $this->description = $description;
-    }
-
-    public function show($type) {
-        if ($type == 'name') {
-            return $this->name;
-        } elseif ($type == 'description') {
-            return $this->description;
-        }
-        return [];
-    }
-}
-
-class Book {
-    public $ISBN;
-    public $title;
-    public $description;
-    public $category;
-    public $language;
-    public $numberOfPage;
-    public $author;
-    public $publisher;
-
-    public function __construct($ISBN, $title, $description, $category, $language, $numberOfPage, $author, $publisher) {
-        $this->ISBN = $ISBN;
-        $this->title = $title;
-        $this->description = $description;
-        $this->category = $category;
-        $this->language = $language;
-        $this->numberOfPage = $numberOfPage;
-        $this->author = $author;
-        $this->publisher = $publisher;
-    }
-
-    public function showAll() {
-        return [
-            'ISBN' => $this->ISBN,
-            'Title' => $this->title,
-            'Description' => $this->description,
-            'Category' => $this->category,
-            'Language' => $this->language,
-            'Number of Pages' => $this->numberOfPage,
-            'Author' => $this->author,
-            'Publisher' => $this->publisher
-        ];
-    }
-
-    public function detail($ISBN) {
-        if ($this->ISBN == $ISBN) {
-            return $this->showAll();
-        }
-        return [];
-    }
-}
-
-class Publisher {
-    public $name;
-    public $address;
-    private $phone;
-
-    public function __construct($name, $address) {
-        $this->name = $name;
-        $this->address = $address;
-    }
-
-    public function setPhone($phone) {
-        $this->phone = $phone;
-    }
-
-    public function getPhone() {
-        return $this->phone;
-    }
-}
+require_once 'Author.php';
+require_once 'Book.php';
+require_once 'Publisher.php';
 
 $author = new Author("J.K. Rowling", "British author, best known for the Harry Potter series.");
 
