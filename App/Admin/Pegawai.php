d4634dee325c4eb448431390c170a5f1e45ac656
Argamayer <160709033+Argamayer@users.noreply.github.com>
2024-09-18 19:54:50 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/Pegawai.php
@@ -0,0 +1,33 @@
+<?php
+
+class Pegawai {
+    public int $nip;
+    public string $nama;
+    protected int $no_hp;
+    public string $alamat;
+
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat) {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $no_hp;
+        $this->alamat = $alamat;
+    }
+
+    public function cekIn(): bool {
+        // Implementasi logika cekIn
+        return true;
+    }
+
+    public function cekOut(): bool {
+        // Implementasi logika cekOut
+        return true;
+    }
+
+    public function getNoHp(): int {
+        return $this->no_hp;
+    }
+
+    public function setNoHp(int $no_hp): void {
+        $this->no_hp = $no_hp;
+    }
+}
